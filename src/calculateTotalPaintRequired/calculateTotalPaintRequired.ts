0e96eda6aa1bc17d86e20fac2ab893b969afc2f5
yashp0906 <patelyashj9698@gmail.com>
2025-04-09 15:28:10 -0400

    Add code into calculateTotalPaintRequired

--- src/calculateTotalPaintRequired/calculateTotalPaintRequired.ts
+++ src/calculateTotalPaintRequired/calculateTotalPaintRequired.ts
@@ -1,2 +1,12 @@
-
+export function calculateTotalPaintRequired(
+    width: number,
+    height: number,
+    numberOfCopies: number
+  ): number {
+    const rectangleArea = width * height;
+    const centerTriangleArea = (width * height) / 6; 
+    const totalAreaPerCopy = rectangleArea + centerTriangleArea;
+    return totalAreaPerCopy * numberOfCopies;
+  }
+  
   
