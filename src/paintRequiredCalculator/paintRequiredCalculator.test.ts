f3ee1be81c6c271a956cf0cfec82ddef7f817060
yashp0906 <patelyashj9698@gmail.com>
2025-04-09 14:57:41 -0400

    Implement testcase for fractional result if area is not evenly divisible in paintRequiredCalculator

--- src/paintRequiredCalculator/paintRequiredCalculator.test.ts
+++ src/paintRequiredCalculator/paintRequiredCalculator.test.ts
@@ -4,5 +4,7 @@ describe("paint required calculator", () => {
   test('should return NaN if both area and coveragePerUnit are 0', () => {
     expect(paintRequiredCalculator(0, 0)).toBeNaN();
   });
-  
+  test('should return fractional result if area is not evenly divisible', () => {
+    expect(paintRequiredCalculator(55, 20)).toBeCloseTo(2.75, 2);
+  });  
 });
