0ce47a0c06976ec5c461d1730c1a93c75a58e8de
yashp0906 <patelyashj9698@gmail.com>
2025-04-09 15:33:18 -0400

    Add testcase for 0 number of copies into calculateTotalPaintRequired

--- src/calculateTotalPaintRequired/calculateTotalPaintRequired.test.ts
+++ src/calculateTotalPaintRequired/calculateTotalPaintRequired.test.ts
@@ -3,5 +3,7 @@ describe('convertAreaToSquareFeet', () => {
     test('should calculate correct paint for 1 copy of 3x2 rectangle', () => {
         expect(calculateTotalPaintRequired(3, 2, 1)).toBeCloseTo(7, 2);
       });
-
+      test('should return 0 if number of copies is 0', () => {
+        expect(calculateTotalPaintRequired(3, 2, 0)).toBe(0);
+      });
 });
