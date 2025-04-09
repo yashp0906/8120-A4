728350e3668be4d759737408cb8cc4be9a524663
yashp0906 <patelyashj9698@gmail.com>
2025-04-09 14:51:41 -0400

    Implement testcase for fractional time if not evenly divisible in estimatePaintingTime

--- src/estimatePaintingTime/estimatePaintingTime.test.ts
+++ src/estimatePaintingTime/estimatePaintingTime.test.ts
@@ -4,5 +4,8 @@ describe('estimatePaintingTime', () => {
   test('should return Infinity if coveragePerHour is 0', () => {
     expect(estimatePaintingTime(100, 0)).toBe(Infinity);
   });
+  test('should return fractional time if not evenly divisible', () => {
+    expect(estimatePaintingTime(150, 40)).toBeCloseTo(3.75, 2);
+  });
 });
 
