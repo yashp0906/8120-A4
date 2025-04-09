0f48195781a3e5273002eee9c6bc2d189bb13424
yashp0906 <patelyashj9698@gmail.com>
2025-04-09 14:24:00 -0400

    Implement testcase for width 3 and height 4 in calculateCanvasDiagonal

--- src/calculateCanvasDiagonal/calculateCanvasDiagonal.test.ts
+++ src/calculateCanvasDiagonal/calculateCanvasDiagonal.test.ts
@@ -4,4 +4,7 @@ describe('calculateCanvasDiagonal', () => {
   test('should return 0 for length and width of 0', () => {
     expect(calculateCanvasDiagonal(0,0)).toBe(0);
   });
+  test('should return 5 for width 3 and height 4 (3-4-5 triangle)', () => {
+    expect(calculateCanvasDiagonal(3, 4)).toBe(5);
+  });
 });
