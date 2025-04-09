c319b063a4f046a459a671b65ee9d7191044f179
yashp0906 <patelyashj9698@gmail.com>
2025-04-09 14:46:50 -0400

    Implement testcase for 10 square meter in convertAreaToSquareFeet

--- src/convertAreaToSquareFeet/convertAreaToSquareFeet.test.ts
+++ src/convertAreaToSquareFeet/convertAreaToSquareFeet.test.ts
@@ -4,4 +4,7 @@ describe('convertAreaToSquareFeet', () => {
   test('should return 0 when area in square meters is 0', () => {
     expect(convertAreaToSquareFeet(0)).toBe(0);
   });
+  test('should correctly convert 10 square meters to square feet', () => {
+    expect(convertAreaToSquareFeet(10)).toBeCloseTo(107.639, 3);
+  });
 });
